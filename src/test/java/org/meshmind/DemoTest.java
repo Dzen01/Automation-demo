5de8f62bd117dcd1ba1c5bc0be4fab5b076a8d60
dzenan <dzenanlukovac0@gmail.com>
2023-07-28 14:54:38 +0200

    First commit to new branch

---
+++ src/test/java/org/meshmind/DemoTest.java
@@ -0,0 +1,23 @@
+package org.meshmind;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+    @BeforeClass
+    public void demoBeforeClass(){
+
+    }
+    @Test
+    public void demoTest1(){
+
+    }
+    @Test
+    public void demoTest2(){
+
+    }
+    @Test
+    public void demoTest3(){
+
+    }
+}
