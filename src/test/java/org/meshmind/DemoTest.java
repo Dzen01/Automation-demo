727ab2c5bc3bd31b13232f7da1c2a902cd9c62e2
dzenan <dzenanlukovac0@gmail.com>
2023-07-28 15:04:01 +0200

    Second Commit for testing branch

--- src/test/java/org/meshmind/DemoTest.java
+++ src/test/java/org/meshmind/DemoTest.java
@@ -6,18 +6,30 @@ import org.testng.annotations.Test;
 public class DemoTest {
     @BeforeClass
     public void demoBeforeClass(){
-
+        System.out.println("Before Class");
     }
     @Test
     public void demoTest1(){
-
+        System.out.println("Demo Test 1");
     }
     @Test
     public void demoTest2(){
-
+        System.out.println("Demo Test 2");
     }
     @Test
     public void demoTest3(){
-
+        System.out.println("Demo Test 3");
+    }
+    @Test
+    public void newDemoTest1(){
+        System.out.println("New Demo Test 1");
+    }
+    @Test
+    public void newDemoTest2(){
+        System.out.println("New Demo Test 2");
+    }
+    @Test
+    public void newDemoTest3(){
+        System.out.println("New Demo Test 3");
     }
 }
